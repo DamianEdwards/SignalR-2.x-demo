1d655958975ba3f4d4f84f4c3883d8c47e55713d
DamianEdwards <damian@damianedwards.com>
2013-11-18 23:27:50 -0800

    Added strongly typed hubs demo

---
+++ SignalR2x/Web/StronglyTypedHubs/StrongHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace Web.StronglyTypedHubs
+{
+    public class StrongHub : Hub<IClient>
+    {
+        public void Send(string message)
+        {
+            Clients.All.NewMessage(message);
+        }
+    }
+}
