8bf7e64dd5807b4727610cca2f5945e05ac4c4f9
DamianEdwards <damian@damianedwards.com>
2013-11-18 23:48:37 -0800

    Added Hub progress demo

---
+++ SignalR2x/Web/HubProgress/ProgressHub.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace Web.HubProgress
+{
+    public class ProgressHub : Hub
+    {
+        public async Task<string> DoLongRunningThing(IProgress<int> progress)
+        {
+            for (int i = 0; i <= 100; i+=5)
+            {
+                await Task.Delay(200);
+                progress.Report(i);
+            }
+
+            return "Job complete!";
+        }
+    }
+}
